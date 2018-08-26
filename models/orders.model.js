54a4993387300c853629da965680806faa02573b
Yuho <yhchiu@snsoft.my>
2018-08-26 11:06:27 +0800

    fix total

--- models/orders.model.js
+++ models/orders.model.js
@@ -4,17 +4,9 @@ mongoose.set("debug", true)
 const SCHEMA = mongoose.Schema
 const ordersSchema = new SCHEMA({
   _id:        String,
-  totalToPay: { type: Number, get: getPrice, set: setPrice },
+  totalToPay: { type: Number },
   orders:     [{ type: mongoose.Schema.ObjectId, ref: "Request" }],
 })
-
-function getPrice(num) {
-  return (num / 100).toFixed(2)
-}
-
-function setPrice(num) {
-  return num * 100
-}
 
 export default mongoose.model("orders", ordersSchema)
 
